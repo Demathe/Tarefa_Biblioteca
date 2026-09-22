d035966838dd60e7f484c70f976d31d4a1a4e468
ramon-heerdt <137829996+ramon-heerdt@users.noreply.github.com>
2026-09-22 15:47:27 -0300

    Lorenzo

--- livros.py
+++ livros.py
@@ -2,3 +2,12 @@ import sqlite3
 
 conn = sqlite3.connect(biblioteca.db)
 
+conn.execute("DROP TABLE IF EXISTS livros")
+
+conn.execute("CREATE TABLE livros(\
+    id INTEGER PRIMARY KEY AUTOINCREMENT, \
+    id_autor INTEGER REFERENCES autores(id),\
+    id_autor INTEGER REFERENCES editoras(id),\
+    ano_publicacao INTEGER NOT NULL,\
+    edicao INTEGER DEFAULT(1),\
+    DISPONIVEL ")
