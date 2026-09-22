da5606f65d3b299bae4e50e03d4a8a2b5e8354e8
ramon-heerdt <137829996+ramon-heerdt@users.noreply.github.com>
2026-09-22 14:26:24 -0300

    Lorenzo

---
+++ menu.py
@@ -0,0 +1,25 @@
+#menu da atividade
+#lorenzo
+
+
+while(True):
+
+    print("     ===MENU===      \n(1) Adicionar usuario \n(2) Adicionar autor no sistema \n(3)adicionar editoras ao sistema \n(4) adicionar livro a biblioteca \n(5) emprestimo \(6) adicionar livros do emprestimo\n (0) sair")
+
+    menu = input("O que voce desaja fazer: ")
+
+
+    if menu == 1:
+        A
+    elif menu == 2:
+        A
+    elif menu ==3:
+        A
+    elif menu ==4:
+        A
+    elif menu ==5:
+        A
+    elif menu ==6:
+        A
+    else:
+        break
